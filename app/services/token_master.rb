ece2af5cda501d2b9d2dbf0661c0e3ac80339139
Matic Jurglic <matic@jurglic.si>
2016-06-08 22:27:46 +0200

    make sure env variable exists

--- app/services/token_master.rb
+++ app/services/token_master.rb
@@ -2,6 +2,6 @@ class TokenMaster
   AVAILABLE_TOKENS = %w(GITHUB_AUTH_1 GITHUB_AUTH_2 GITHUB_AUTH_3 GITHUB_AUTH_4)
 
   def self.get_token
-    ENV[AVAILABLE_TOKENS.sample]
+    ENV.fetch(AVAILABLE_TOKENS.sample)
   end
 end
