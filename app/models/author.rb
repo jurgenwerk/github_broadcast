b90c9f9cd2a083095dc4cb3176dce3f0f7081507
Matic Jurglic <matic@jurglic.si>
2016-06-11 17:52:27 +0200

    add location attr to author

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,7 @@ class Author
   field :name, type: String
   field :longitude, type: String
   field :latitude, type: String
+  field :location, type: String
 
   index({ name: 1 }, { unique: true, name: "author_name_index" })
 end
